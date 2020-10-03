e9765bf534289a93a4578c3b45f58cc737996edb
Ambika K <ambikak10@gmail.com>
2020-10-02 23:31:16 -0700

    installed axios, nodemon backend server intialized

--- backend/data/products.js
+++ backend/data/products.js
@@ -80,3 +80,4 @@ const products = [
 ]
 
 export default products
+// module.exports = products
--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -71,9 +71,9 @@ const Rating = ({value, text, color}) => {
 Rating.defaultProps ={
   color: "#f8e825"
 }
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string
-};
+// Rating.propTypes = {
+//   value: PropTypes.number.isRequired,
+//   text: PropTypes.string.isRequired,
+//   color: PropTypes.string
+// };
 export default Rating
