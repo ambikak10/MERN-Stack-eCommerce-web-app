c7bc7f4d900d437b1f792a938b454fc7cb62dd40
Ambika K <ambikak10@gmail.com>
2020-10-03 17:09:41 -0700

    postman testing of routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -15,10 +15,9 @@ router.get("/", asyncHandler(async (req, res) => {
 // @route  GET /api/products/:id
 // @access Public
 router.get("/:id", asyncHandler(async(req, res) => {
-  const product = await Product.findById(req.params.id);
-  console.log(product)
+  const product = await Product.findById(req.params.id)
   if(product) {
-    res.json(product);
+    res.json(product)
   } else {
     res.status(404).json({message:'Product not found'})
   } 
