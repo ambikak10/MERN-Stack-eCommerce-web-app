d7b05d92fbf3fed71dda2ce4488daf41fdf9451d
Ambika K <ambikak10@gmail.com>
2020-10-03 18:27:23 -0700

    custom error handling with middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -19,7 +19,9 @@ router.get("/:id", asyncHandler(async(req, res) => {
   if(product) {
     res.json(product)
   } else {
-    res.status(404).json({message:'Product not found'})
+    res.status(404)
+    // .json({message:'Product not found'})
+    throw new Error('Product not found')
   } 
 }));
 
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
+
 // app.get('/api/products', (req, res) => {
 //   res.json(products)
 // });
@@ -23,7 +25,11 @@ app.get('/', (req, res) => {
 //   const product = products.find(p => p._id === req.params.id)
 //   res.json(product)
 // })
+
 app.use('/api/products', productRoutes)
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000
 
